9a71805a6e9781850866ee49c9fa62c7c84d1014
Paul Hebert <paul@cloudfour.com>
2020-04-17 12:45:37 -0700

    Improve Framer X project

--- components.framerfx/code/C4Button.tsx
+++ components.framerfx/code/C4Button.tsx
@@ -3,16 +3,44 @@ import { ControlType, addPropertyControls } from "framer"
 import { C4Button as _C4Button } from "../../react-components/dist"
 
 export const C4Button = (props: any) => (
-    <_C4Button {...props}>{props.slot}</_C4Button>
+    <_C4Button height="60" width="100" {...props}>
+        {props.slot}
+    </_C4Button>
 )
 
 addPropertyControls(C4Button, {
     slot: {
         type: ControlType.String,
-        defaultValue: "Hello world",
+        defaultValue: "Button",
+    },
+    buttonClass: {
+        type: ControlType.String,
+        defaultValue: null,
+    },
+    tag: {
+        type: ControlType.Enum,
+        defaultValue: "button",
+        options: ["a", "button"],
     },
     disabled: {
         type: ControlType.Boolean,
         defaultValue: false,
+        hidden(props) {
+            return props.tag === "a"
+        },
+    },
+    type: {
+        type: ControlType.String,
+        defaultValue: "button",
+        hidden(props) {
+            return props.tag === "a"
+        },
+    },
+    href: {
+        type: ControlType.String,
+        defaultValue: "href",
+        hidden(props) {
+            return props.tag !== "a"
+        },
     },
 })
