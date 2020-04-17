8d402bd951ba7c4effdf6086bf6b5cc866c12f3f
Paul Hebert <paul@cloudfour.com>
2020-04-17 13:37:47 -0700

    Use C4Heading in React App

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,16 +1,17 @@
 import React from "react";
 import "./App.css";
-import { C4Button } from "../../react-components";
+import { C4Button, C4Heading } from "../../react-components";
 
 function App() {
   return (
     <div className="App">
-      <h1>A React App Consuming Stencil Components</h1>
+      <C4Heading>A React App Consuming Stencil Components</C4Heading>
 
       <p>
         This button is a web component I created using stencil-js and exported
         for use in a React app.
       </p>
+
       <C4Button>Hello world!</C4Button>
     </div>
   );
