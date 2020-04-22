7e74e9215915d7fe99d46bc9e1b8c92d772c6a08
Paul Hebert <paul@cloudfour.com>
2020-04-22 10:39:27 -0700

    Add Web Components example from storybook repo

---
+++ storybook/.storybook/main.js
@@ -0,0 +1,12 @@
+module.exports = {
+  addons: [
+    '@storybook/addon-docs',
+    '@storybook/addon-a11y',
+    '@storybook/addon-actions',
+    '@storybook/addon-backgrounds',
+    '@storybook/addon-knobs',
+    '@storybook/addon-links',
+    '@storybook/addon-storysource',
+    '@storybook/addon-viewport',
+  ],
+};
