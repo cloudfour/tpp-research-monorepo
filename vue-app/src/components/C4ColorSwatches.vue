1af2c8ffbfac512904715657fb746fda7841df3f
Paul Hebert <paul@cloudfour.com>
2020-04-23 14:52:13 -0700

    Add first component wrappers to Vue app

---
+++ vue-app/src/components/C4ColorSwatches.vue
@@ -0,0 +1,18 @@
+<template>
+  <c4-color-swatches
+    :radio-name.prop='radioName'
+    :colors-data.prop='colors'
+    @colorChanged="emitEvent"
+  ></c4-color-swatches>
+</template>
+
+<script>
+export default {
+  props: ['radioName', 'colors'],
+  methods: {
+    emitEvent(event) {
+      this.$emit('colorChanged', event.detail);
+    },
+  },
+};
+</script>
---
+++ vue-app/src/components/C4Container.vue
@@ -0,0 +1,14 @@
+<template>
+  <c4-container
+    :container-class.prop='containerClass'
+    :content-class.prop='contentClass'
+  >
+    <slot />
+  </c4-container>
+</template>
+
+<script>
+export default {
+  props: ['contentClass', 'containerClass'],
+};
+</script>
