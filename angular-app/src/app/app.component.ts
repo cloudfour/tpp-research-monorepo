a32aaee28a7c1c9ef19edee1f495aee948758c4c
Paul Hebert <paul@cloudfour.com>
2020-04-23 15:48:12 -0700

    Remove unneeded files

--- angular-app/src/app/app.component.ts
+++ angular-app/src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['../../../stencil-components/src/css/base.css']
 })
 export class AppComponent {
   title = 'angular-app';
