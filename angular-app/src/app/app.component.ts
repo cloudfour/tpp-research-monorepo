4979e3826b8d630699399551762fa440416b099f
Paul Hebert <paul@cloudfour.com>
2020-05-01 15:19:42 -0700

    Add more components to angular app

--- angular-app/src/app/app.component.ts
+++ angular-app/src/app/app.component.ts
@@ -5,5 +5,17 @@ import { Component } from "@angular/core";
   templateUrl: "./app.component.html",
 })
 export class AppComponent {
-  title = "my-app";
+  selectedColor = "Cloud Blue";
+
+  colors = [
+    { hex: "#215cca", name: "Cloud Blue", id: "123" },
+    { hex: "#158466", name: "PNW Green", id: "124" },
+    { hex: "#d9118f", name: "PWA Pink", id: "125" },
+    { hex: "#f27041", name: "Orange", id: "126" },
+    { hex: "#485968", name: "Grey", id: "127" },
+  ];
+
+  changeColor(e) {
+    this.selectedColor = e.detail.name;
+  }
 }
