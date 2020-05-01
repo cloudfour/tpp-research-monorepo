6310e6e9d3fa749792ac6d0ba57acceb30c2b347
Paul Hebert <paul@cloudfour.com>
2020-05-01 14:27:23 -0700

    Add CSS to Angular app

--- copy-components.js
+++ copy-components.js
@@ -1,4 +1,5 @@
 var copydir = require("copy-dir");
 
-copydir("stencil-components/dist", "express/public/stencil", {});
-copydir("stencil-components/src/css", "express/public/stencil/css", {});
+copydir("stencil-components/dist", "express-app/public/stencil", {});
+copydir("stencil-components/src/css", "express-app/public/stencil/css", {});
+copydir("stencil-components/src/css", "angular-app/src/assets/css", {});
