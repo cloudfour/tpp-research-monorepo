410b294ba71c4a3751c066bd3b9b47e736765374
Paul Hebert <paul@cloudfour.com>
2020-05-06 11:36:19 -0700

    Copy stencil files to storyblok instance

--- copy-components.js
+++ copy-components.js
@@ -2,4 +2,8 @@ var copydir = require("copy-dir");
 
 copydir("stencil-components/dist", "express-app/public/stencil", {});
 copydir("stencil-components/src/css", "express-app/public/stencil/css", {});
+
+copydir("stencil-components/dist", "cms-storyblok/public/stencil", {});
+copydir("stencil-components/src/css", "cms-storyblok/public/stencil/css", {});
+
 copydir("stencil-components/src/css", "angular-app/src/assets/css", {});
