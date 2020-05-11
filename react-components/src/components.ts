25afc29e12b279b9dae3d333c329f98844b55067
Paul Hebert <paul@cloudfour.com>
2020-05-11 16:42:05 -0700

    Add sizing utilities

--- react-components/src/components.ts
+++ react-components/src/components.ts
@@ -1,37 +1,16 @@
 /* eslint-disable */
 /* tslint:disable */
 /* auto-generated react proxies */
-import { createReactComponent } from "./react-component-lib";
+import { createReactComponent } from './react-component-lib';
 
-import { JSX } from "../../stencil-components";
+import { JSX } from '../../stencil-components';
 
-import {
-  defineCustomElements,
-  applyPolyfills,
-} from "../../stencil-components/loader";
+import { defineCustomElements, applyPolyfills } from '../../stencil-components/loader';
 
 applyPolyfills().then(() => defineCustomElements());
-export const C4Button = /*@__PURE__*/ createReactComponent<
-  JSX.C4Button,
-  HTMLC4ButtonElement
->("c4-button");
-export const C4ColorSwatches = /*@__PURE__*/ createReactComponent<
-  JSX.C4ColorSwatches,
-  HTMLC4ColorSwatchesElement
->("c4-color-swatches");
-export const C4Container = /*@__PURE__*/ createReactComponent<
-  JSX.C4Container,
-  HTMLC4ContainerElement
->("c4-container");
-export const C4Heading = /*@__PURE__*/ createReactComponent<
-  JSX.C4Heading,
-  HTMLC4HeadingElement
->("c4-heading");
-export const C4RadioButtons = /*@__PURE__*/ createReactComponent<
-  JSX.C4RadioButtons,
-  HTMLC4RadioButtonsElement
->("c4-radio-buttons");
-export const C4StarRating = /*@__PURE__*/ createReactComponent<
-  JSX.C4StarRating,
-  HTMLC4StarRatingElement
->("c4-star-rating");
+export const C4Button = /*@__PURE__*/createReactComponent<JSX.C4Button, HTMLC4ButtonElement>('c4-button');
+export const C4ColorSwatches = /*@__PURE__*/createReactComponent<JSX.C4ColorSwatches, HTMLC4ColorSwatchesElement>('c4-color-swatches');
+export const C4Container = /*@__PURE__*/createReactComponent<JSX.C4Container, HTMLC4ContainerElement>('c4-container');
+export const C4Heading = /*@__PURE__*/createReactComponent<JSX.C4Heading, HTMLC4HeadingElement>('c4-heading');
+export const C4RadioButtons = /*@__PURE__*/createReactComponent<JSX.C4RadioButtons, HTMLC4RadioButtonsElement>('c4-radio-buttons');
+export const C4StarRating = /*@__PURE__*/createReactComponent<JSX.C4StarRating, HTMLC4StarRatingElement>('c4-star-rating');
