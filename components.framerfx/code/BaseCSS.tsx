70c0ac9d07fdb89b5c5adf42c734b975496e4749
Paul Hebert <paul@cloudfour.com>
2020-05-11 15:19:17 -0700

    Add fonts to Framer

--- components.framerfx/code/BaseCSS.tsx
+++ components.framerfx/code/BaseCSS.tsx
@@ -1,7 +1,8 @@
 // This loads up our global CSS file. It doesn't actually need to be used in your designs.
 // You could load theme files to overwrite this.
-import "../../stencil-components/src/assets/css/custom-props.css";
-import "../../stencil-components/src/assets/css/document-styles.css";
+import "../../stencil-components/src/assets/css/custom-props.css"
+import "../../stencil-components/src/assets/css/document-styles.css"
+import "../../stencil-components/src/assets/css/fonts.css"
 
 // Uncomment this to apply an (ugly) theme to components
 // import "../../stencil-components/src/assets/css/theme-example.css"
