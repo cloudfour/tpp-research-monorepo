5a6646194c3b7674f9448a02314b0db880215539
Paul Hebert <paul@cloudfour.com>
2020-05-27 10:34:14 -0700

    Add copy task for Ember

--- copy-stencil-output.js
+++ copy-stencil-output.js
@@ -21,13 +21,16 @@ copydir(
 // Copy CSS and fonts to Angular app
 copydir("stencil-components/src/assets", "angular-app/src/assets/stencil", {});
 
+// Copy CSS and fonts to Ember app
+copydir("stencil-components/src/assets", "ember-app/public/assets/stencil", {});
+
 // Copy CSS and fonts to React app
 copydir("stencil-components/src/assets", "react-app/public/assets/stencil", {});
 
 // Copy CSS and fonts to Vue app
 copydir("stencil-components/src/assets", "vue-app/static/assets/stencil", {});
 
-// Copy CSS and fonts to Storybobok
+// Copy CSS and fonts to Storybook
 copydir("stencil-components/src/assets", "storybook/public/assets/stencil", {});
 
 // Other apps import the required files via relative paths so don't need copies.
