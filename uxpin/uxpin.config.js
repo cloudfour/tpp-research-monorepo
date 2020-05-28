1621ceee6d637a93374590bf6aeb705caf0b3e91
Paul Hebert <paul@cloudfour.com>
2020-05-28 10:09:49 -0700

    Tidy up config file

--- uxpin/uxpin.config.js
+++ uxpin/uxpin.config.js
@@ -3,11 +3,10 @@ module.exports = {
     categories: [
       {
         name: "General",
-        include: ["src/c4-button/c4-button.jsx"],
-        include: ["src/c4-star-rating/c4-star-rating.jsx"],
+        include: ["src/c4-button/c4-button.jsx", "src/c4-star-rating/c4-star-rating.jsx"],
       },
     ],
     webpackConfig: "webpack.config.js",
   },
-  name: "New Design System",
+  name: "Cloud Four System",
 };
