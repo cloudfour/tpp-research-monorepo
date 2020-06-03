b391f33cd16557896bf05c9ee0f8501b717d7761
Paul Hebert <paul@cloudfour.com>
2020-06-03 16:23:20 -0700

    Create framer-specific text component for prototyping

---
+++ components.framerfx/code/C4TextLine.tsx
@@ -0,0 +1,38 @@
+import * as React from "react"
+import { ControlType, addPropertyControls } from "framer"
+import { sizeSelectorPresets, sizeToUtility } from "./Utilities"
+import { C4Text } from "../../react-components/dist"
+
+export const C4TextLine = (props: any) => {
+    const fontStyles = {
+        fontWeight: props.weight,
+        fontStyle: props.italic ? "italic" : "normal",
+    }
+    return (
+        <C4Text className={sizeToUtility(props.size)} style={fontStyles}>
+            <p>{props.slot}</p>
+        </C4Text>
+    )
+}
+
+addPropertyControls(C4TextLine, {
+    slot: {
+        type: ControlType.String,
+        defaultValue: "Text",
+    },
+    size: {
+        type: ControlType.Number,
+        ...sizeSelectorPresets,
+    },
+    italic: {
+        type: ControlType.Boolean,
+        defaultValue: false,
+    },
+    weight: {
+        type: ControlType.Number,
+        min: 200,
+        max: 900,
+        step: 1,
+        defaultValue: 400,
+    },
+})
