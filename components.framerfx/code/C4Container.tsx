eadf0e7e2724911efcf1e2ee22e65c3555a6b15c
Paul Hebert <paul@cloudfour.com>
2020-06-04 11:35:32 -0700

    add gap to framer grid

--- components.framerfx/code/C4Container.tsx
+++ components.framerfx/code/C4Container.tsx
@@ -16,7 +16,7 @@ export const C4Container = (props: any) => {
 
     return (
         <_C4Container {...props} style={frameStyles}>
-            <Stack alignment="start" width="100%" height="100%" gap={0}>
+            <Stack alignment="start" width="100%" height="100%" gap={16}>
                 {props.slot}
             </Stack>
         </_C4Container>
