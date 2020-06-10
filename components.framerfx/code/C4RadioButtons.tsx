41f7f10c1dbc21b7acc8251503f4a1c436c097d4
Paul Hebert <paul@cloudfour.com>
2020-06-10 16:29:39 -0700

    Add size option to C4Text

--- components.framerfx/code/C4RadioButtons.tsx
+++ components.framerfx/code/C4RadioButtons.tsx
@@ -1,43 +1,43 @@
-import * as React from "react";
-import { ControlType, addPropertyControls } from "framer";
-import { C4RadioButtons as _C4RadioButtons } from "../../react-components/dist";
-import { sizeSelectorPresets, sizeToUtility } from "./Utilities";
+import * as React from "react"
+import { ControlType, addPropertyControls } from "framer"
+import { C4RadioButtons as _C4RadioButtons } from "../../react-components/dist"
+import { sizeSelectorPresets, sizeToUtility } from "./Utilities"
 
 export const C4RadioButtons = (props: any) => {
-  const optionsArray = props.options
-    ? props.options.map((name) => {
-        return {
-          name: name,
-          id: "xxx",
-        };
-      })
-    : [];
+    const optionsArray = props.options
+        ? props.options.map(name => {
+              return {
+                  name: name,
+                  id: "xxx",
+              }
+          })
+        : []
 
-  return (
-    <_C4RadioButtons
-      optionsData={optionsArray}
-      // We randomize the radio name so if this is used in multiple designs it's unique each time.
-      // This ensures the first option is checked.
-      radioName={props.radioName + Math.random().toString()}
-      className={sizeToUtility(props.size)}
-    />
-  );
-};
+    return (
+        <_C4RadioButtons
+            optionsData={optionsArray}
+            // We randomize the radio name so if this is used in multiple designs it's unique each time.
+            // This ensures the first option is checked.
+            radioName={props.radioName + Math.random().toString()}
+            className={sizeToUtility(props.size)}
+        />
+    )
+}
 
 addPropertyControls(C4RadioButtons, {
-  radioName: {
-    type: ControlType.String,
-    defaultValue: "sizes",
-  },
-  options: {
-    type: ControlType.Array,
-    propertyControl: {
-      type: ControlType.String,
+    radioName: {
+        type: ControlType.String,
+        defaultValue: "sizes",
     },
-    defaultValue: ["S", "M", "L", "XL"],
-  },
-  size: {
-    type: ControlType.Number,
-    ...sizeSelectorPresets,
-  },
-});
+    options: {
+        type: ControlType.Array,
+        propertyControl: {
+            type: ControlType.String,
+        },
+        defaultValue: ["S", "M", "L", "XL"],
+    },
+    size: {
+        type: ControlType.Number,
+        ...sizeSelectorPresets,
+    },
+})
--- components.framerfx/code/C4Text.tsx
+++ components.framerfx/code/C4Text.tsx
@@ -1,18 +1,23 @@
-import * as React from "react";
-import { ControlType, addPropertyControls } from "framer";
-import { C4Text as _C4Text } from "../../react-components/dist";
-import Markdown from "markdown-to-jsx";
-import { render } from "react-dom";
+import * as React from "react"
+import { ControlType, addPropertyControls } from "framer"
+import { C4Text as _C4Text } from "../../react-components/dist"
+import Markdown from "markdown-to-jsx"
+import { render } from "react-dom"
+import { sizeSelectorPresets, sizeToUtility } from "./Utilities"
 
 export const C4Text = (props: any) => (
-  <_C4Text>
-    <Markdown>{props.slot}</Markdown>
-  </_C4Text>
-);
+    <_C4Text className={sizeToUtility(props.size)}>
+        <Markdown>{props.slot}</Markdown>
+    </_C4Text>
+)
 
 addPropertyControls(C4Text, {
-  slot: {
-    type: ControlType.String,
-    defaultValue: "Text",
-  },
-});
+    slot: {
+        type: ControlType.String,
+        defaultValue: "Text",
+    },
+    size: {
+        type: ControlType.Number,
+        ...sizeSelectorPresets,
+    },
+})
