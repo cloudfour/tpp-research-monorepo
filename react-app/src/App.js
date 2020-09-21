3febd83fe145f4ce675bedd94ccc86cb7fa87ed7
Scott Vandehey <scott@cloudfour.com>
2020-09-21 15:08:18 -0700

    Standardize Landing Page Between Apps
    This commit standardizes the contents of the landing page between the
    Express, React, and Angular apps.

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,20 +1,43 @@
 import React from "react";
 
-import { C4Button, C4Heading, C4Container } from "../../react-components";
+import {
+  C4Button,
+  C4Heading,
+  C4Container,
+  C4StarRating,
+} from "../../react-components";
 import { SwatchWrapper } from "./components/swatch-wrapper";
 
 function App() {
   return (
     <div className="App">
       <C4Container isProse="true">
-        <C4Heading>A React App Consuming Stencil Components</C4Heading>
+        <C4Heading>React App</C4Heading>
 
         <p>
-          This button is a web component I created using stencil-js and exported
-          for use in a React app.
+          This is a React app built using Stencil web components. The components
+          have been wrapped in React components.
         </p>
 
-        <C4Button>Hello world!</C4Button>
+        <C4Heading tag="h2">Buttons</C4Heading>
+
+        <p>Here are some buttons:</p>
+
+        <C4Button>I'm a c4-button!</C4Button>
+
+        <C4Button buttonClass="secondary">I'm a secondary c4-button!</C4Button>
+
+        <C4Button buttonClass="tertiary">I'm a tertiary c4-button!</C4Button>
+
+        <C4Button disabled="true">I'm a disabled c4-button!</C4Button>
+
+        <C4Button tag="a">I'm actually a link!</C4Button>
+
+        <div>
+          <C4Heading tag="h2">Star Rating</C4Heading>
+
+          <C4StarRating rating="4.6" style={{ fontSize: "3em" }}></C4StarRating>
+        </div>
 
         <SwatchWrapper
           radioName="colors"
@@ -26,15 +49,18 @@ function App() {
             { hex: "#485968", name: "Grey", id: "127" },
           ]}
         ></SwatchWrapper>
-      </C4Container>
-      <C4Container isProse="true" isDark="true">
-        <C4Heading tag="h2">A Themed Container</C4Heading>
 
         <p>
-          This container is using the <code>dark</code> theme modifier.
+          The color swatches emit an event when the color is changed. This page
+          is using React event binding.
         </p>
-
-        <C4Button>Hello world!</C4Button>
+      </C4Container>
+      <C4Container isProse="true" isTall="true" isDark="true">
+        <C4Heading tag="h1">Dark Theme</C4Heading>
+        <p>
+          This container has the dark theme. The dark theme updates custom
+          properties to change colors.
+        </p>
       </C4Container>
     </div>
   );
