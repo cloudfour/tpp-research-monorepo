3570eb0e8c49ceee83455f39cfd66a5de79ccf75
Paul Hebert <paul@cloudfour.com>
2020-12-07 10:18:05 -0800

    Remove shadow DOM from  headings

--- stencil-components/src/components/c4-heading/c4-heading.tsx
+++ stencil-components/src/components/c4-heading/c4-heading.tsx
@@ -2,15 +2,14 @@ import { Component, Prop, h } from "@stencil/core";
 
 @Component({
   tag: "c4-heading",
-  styleUrl: "c4-heading.css",
-  shadow: true,
+  styleUrl: "c4-heading.css"
 })
 export class C4Heading {
   @Prop() tag: string = "h1";
 
   render() {
     return (
-      <this.tag>
+      <this.tag className={`c4-heading c4-heading--${this.tag}`}>
         <slot />
       </this.tag>
     );
