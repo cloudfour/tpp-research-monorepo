97fcdf8df4872bcaafc059b8cd278faa866fbfb4
Paul Hebert <paul@cloudfour.com>
2020-12-07 10:14:32 -0800

    Remove Shadow DOM from Details Lockup

--- stencil-components/src/components/c4-details-lockup/c4-details-lockup.tsx
+++ stencil-components/src/components/c4-details-lockup/c4-details-lockup.tsx
@@ -2,22 +2,21 @@ import { Component, h } from "@stencil/core";
 
 @Component({
   tag: "c4-details-lockup",
-  styleUrl: "c4-details-lockup.css",
-  shadow: true,
+  styleUrl: "c4-details-lockup.css"
 })
 export class C4DetailsLockup {
   render() {
     return (
-      <div class="lockup">
-        <div class="header">
+      <div class="c4-lockup">
+        <div class="c4-lockup__header">
           <slot name="header"></slot>
         </div>
 
-        <div class="content">
+        <div class="c4-lockup__content">
           <slot name="content"></slot>
         </div>
 
-        <div class="footer">
+        <div class="c4-lockup__footer">
           <slot name="footer"></slot>
         </div>
       </div>
