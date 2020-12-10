b305cdfb833b982b5b427cc104095ba677cfa8ce
Paul Hebert <paul@cloudfour.com>
2020-12-10 09:33:47 -0800

    Remove Shadow DOM from C4 Text

--- stencil-components/src/components/c4-text/c4-text.tsx
+++ stencil-components/src/components/c4-text/c4-text.tsx
@@ -2,13 +2,12 @@ import { Component, Host, h } from "@stencil/core";
 
 @Component({
   tag: "c4-text",
-  styleUrl: "c4-text.css",
-  shadow: true,
+  styleUrl: "c4-text.css"
 })
 export class C4Text {
   render() {
     return (
-      <Host>
+      <Host class="c4-text">
         <slot></slot>
       </Host>
     );
