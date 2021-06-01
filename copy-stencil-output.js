eca54d748f410c90f14aa6d5a5455dd0b56a45c3
Paul Hebert <paul@cloudfour.com>
2021-06-01 15:28:58 -0700

    Add file to root

--- copy-stencil-output.js
+++ copy-stencil-output.js
@@ -2,6 +2,7 @@ var copydir = require('copy-dir');
 
 // Copy components to Express app
 copydir('stencil-components/dist', 'express-app/public/stencil', {});
+copydir('stencil-components/dist', 'vanilla-html/public/stencil', {});
 
 // Copy components to Storyblok CMS app
 copydir('stencil-components/dist', 'cms-storyblok/public/stencil', {});
---
+++ copy-vanilla-html.js
@@ -0,0 +1,4 @@
+var copydir = require('copy-dir');
+
+// Copy vanilla html to dist
+copydir('vanilla-html', 'dist', {});
